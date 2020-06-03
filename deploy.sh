1ccac6b7070e4b5e180b7447716d2acf797d93e0
Michal Ketner <michal.ketner@pirati.cz>
2020-06-03 18:38:39 -0400

    auto-deploy

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+git checkout -b gh-pages
+git add web
+git -c user.name=$ -c LEKTOR_DEPLOY_USERNAME commit -m init
+git push -f -q https://LEKTOR_DEPLOY_USERNAME:LEKTOR_DEPLOY_PASSWORDY@github.com/LEKTOR_DEPLOY_USERNAME/european-pirateparty.eu-gh-pages gh-pages &>/dev/null
